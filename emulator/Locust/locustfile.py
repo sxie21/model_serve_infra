1d0b5334b0ae1155357f4eec4b1d5aba9ab538c8
sxie21 <378429976@qq.com>
2024-11-07 22:25:16 +0800

    Update locustfile.py

--- emulator/Locust/locustfile.py
+++ emulator/Locust/locustfile.py
@@ -1,15 +1,17 @@
 from locust import HttpUser, task, between
+import os
 import random
 
 class APIUser(HttpUser):
     wait_time = between(1, 2)  # latency bewteen each request
+    target_host = os.getenv('TARGET_HOST', 'http://nginx/test/predict')
 
     @task
     def predict(self):
         x1 = random.uniform(0, 1)
         x2 = random.uniform(0, 1)
         
-        response = self.client.post("/predictions/model_v1", json={"data":[x1,x2]})
+        response = self.client.post(target_host, json={"data":[x1,x2]})
         
         if response.status_code != 200:
             print(f"Request failed with status code {response.status_code}")

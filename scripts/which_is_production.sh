bca1ae8f2a4c4eacceced63f6bd28c979de7bdfe
sxie21 <378429976@qq.com>
2024-11-08 11:27:37 +0800

    Update which_is_production.sh

--- scripts/which_is_production.sh
+++ scripts/which_is_production.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get current route
-current=$(docker-compose exec nginx cat /etc/nginx/nginx.conf | grep "proxy_pass" | head -n 1)
+current=$(docker-compose exec nginx cat /etc/nginx/nginx.conf | sed -n '/location \/predict/,/}/p' | grep "proxy_pass" | head -n 1)
 
 # echo out the production deployment
 if [[ $current = *"green"* ]]; then

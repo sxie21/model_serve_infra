aa0e984e16120d5cd4c6f271df5d5e44d6370f84
sxie21 <378429976@qq.com>
2024-11-20 19:13:12 +0800

    Delete model_serve_infra directory

--- model_serve_infra/jenkins/post.lua
+++
@@ -1,3 +0,0 @@
-wrk.method = "POST"
-wrk.body = '{"data": [0.2, 0.6]}'
-wrk.headers["Content-Type"] = "application/json"
--- model_serve_infra/simulator/kafka_consumer/install_and_run.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install -y nodejs npm
-sudo npm install kafkajs prom-client
-
-nohup  node consumer.js &
